89dd06653e18c03689994a1b531f8611269e61bc
morenodasortejb <morenodasortejb@gmail.com>
2025-02-25 13:41:15 -0300

    Create bundle.js

---
+++ public/dist/bundle.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+        <div class="container">
+            <h1>JOGO DO BICHO - RESULTADOS</h1>
+            <p>Em breve, os resultados serão exibidos aqui...</p>
+        </div>
+    `;
+});
